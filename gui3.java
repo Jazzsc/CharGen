530f820d4efd5dcee06181c8c2e3f852b72438df
ZilvaX <Kevin.x.g@gmail.com>
2015-04-26 11:52:07 +1200

    Stats
    Implemented a stats part of the application. Isn't functional yet though

--- gui3.java
+++ gui3.java
@@ -20,9 +20,12 @@ public class gui3 implements ActionListener {
 	// Titles
 	JLabel dnd;
 	JLabel charDet;
+	JLabel statsLabel;
 	// Character
 	JPanel charPane;
 	JPanel racejobbg;
+	JPanel statsPane;
+	JPanel statsSpine;
 	
 	String race;
 	JLabel raceL;
@@ -50,6 +53,12 @@ public class gui3 implements ActionListener {
 	JLabel flawL;
 	// JTextField flawT;
 	JTextArea flawA;
+	
+	// Stats
+	JLabel strL,dexL,conL,intL,wisL,chaL;
+	JLabel[] statsL = {strL,dexL,conL,intL,wisL,chaL};
+	JTextField strA,dexA,conA,intA,wisA,chaA;
+	JTextField[] statsA = {strA,dexA,conA,intA,wisA,chaA};
 
 	public static void main(String[] args) {
 		// charStart = new charGen();
@@ -67,6 +76,7 @@ public class gui3 implements ActionListener {
 		main = new JFrame("D&D Character Generator");
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		// Set up panels
 		pane = new JPanel(new GridBagLayout());
 		Border blackline = BorderFactory.createLineBorder(Color.black);
 		pane.setBorder(blackline);
@@ -81,6 +91,12 @@ public class gui3 implements ActionListener {
 		
 		racejobbg = new JPanel(new GridLayout(6, 1));
 		racejobbg.setBorder(blackline);
+		
+		statsPane = new JPanel(new GridBagLayout());
+		statsPane.setBorder(blackline);
+		
+		statsSpine = new JPanel();
+		statsSpine.setLayout(new BoxLayout(statsSpine, BoxLayout.PAGE_AXIS));
 
 		// Labels and Buttons
 
@@ -96,8 +112,10 @@ public class gui3 implements ActionListener {
 		dnd.setAlignmentX(Component.CENTER_ALIGNMENT);
 		charDet = new JLabel("Character", JLabel.CENTER);
 		charDet.setAlignmentX(Component.CENTER_ALIGNMENT);
+		statsLabel = new JLabel("Stats", JLabel.CENTER);
+		statsLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
-	
+		// Race Job BG
 		raceL = new JLabel("Race", JLabel.CENTER);
 		raceT = new JTextField(10);
 		raceT.setEditable(false);
@@ -113,6 +131,8 @@ public class gui3 implements ActionListener {
 		bgT.setEditable(false);
 		bgT.setHorizontalAlignment(JTextField.CENTER);
 		
+		
+		// Pane, Personality, Ideals, Bonds and Flaws
 		personaL = new JLabel("Personality: ", JLabel.RIGHT);
 		personaA = new JTextArea(2, 40);
 		personaA.setEditable(false);
@@ -136,10 +156,22 @@ public class gui3 implements ActionListener {
 		flawA.setEditable(false);
 		flawA.setLineWrap(true);
 		flawA.setWrapStyleWord(true);
+		
+		// Stats
+		
+		String[] statsS = {"STR","DEX","CON","INT","WIS","CHA"};
+		for (int i = 0; i < statsL.length; i++){
+			statsL[i] = new JLabel(statsS[i]);
+		}
+		for (int i = 0; i < statsA.length; i++){
+			statsA[i] = new JTextField(2);
+			statsA[i].setEditable(false);
+			statsA[i].setHorizontalAlignment(JTextField.CENTER);
+		}
 
 		
 		
-		
+		// Adding Components
 		racejobbg.add(raceL);
 		racejobbg.add(raceT);
 		racejobbg.add(jobL);
@@ -153,17 +185,13 @@ public class gui3 implements ActionListener {
 		c.gridx = 0;
 		c.gridy = 0;
 		c.gridwidth = 1;
-		//c.weightx = 0.5;
 		charPane.add(racejobbg,c);
 		
 		c.gridx = 1;
 		c.gridy = 0;
-		c.gridwidth = 1;
-		//c.weightx = 0;
+		c.gridwidth = 2;
 		charPane.add(pane, c);
-		
-		
-		
+				
 		c.gridx = 0;
 		c.gridy = 0;
 		pane.add(personaL, c);
@@ -188,19 +216,74 @@ public class gui3 implements ActionListener {
 		c.gridx = 1;
 		c.gridy = 3;
 		pane.add(flawA, c);
+		
+		c.gridx = 4;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		c.ipadx = 20;
+		c.insets = new Insets(0,15,0,0); 
+		charPane.add(statsSpine,c);
+		c.insets = new Insets(0,0,0,0); 
+		
+		statsSpine.add(statsLabel);
+		statsSpine.add(statsPane);
+
+		c.ipadx = 10;
+		c.gridx = 0;
+		c.gridy = 0;
+		
+		/*
+		int tempPos = 0;
+		for (JLabel x:statsL){
+			c.gridx = tempPos%2;
+			c.gridy = tempPos/2;
+			statsPane.add(x,c);
+			tempPos++;
+			
+		}
+		
+		
+		tempPos = 0;
+		for (JTextField x:statsA){
+			c.gridx = tempPos%2;
+			c.gridy = tempPos/2+1;
+			statsPane.add(x,c);
+			tempPos++;
+			
+		}
+		*/
+		
+		
+		for (int i = 0; i<statsL.length; i++){
+			c.gridx = i%2;
+			if (i%2 == 0){
+				c.gridy = i;
+			} else{
+				c.gridy = i-1;
+			}
+			statsPane.add(statsL[i],c);
+			c.gridy = c.gridy + 1;
+			statsPane.add(statsA[i],c);
+			
+		}
+		
+		
 
 		
+		
+		
+
 		mainPane.add(dnd);
 		mainPane.add(charDet);
 		mainPane.add(charPane);
-		//mainPane.add(pane);
 		mainPane.add(genB);
 		main.setContentPane(mainPane);
 
 		// Display the window.
 		main.pack();
 		//main.setSize(699, 685);
-		main.setSize(699, 275);
+		//main.setSize(699, 275);
+		main.setSize(750, 275);
 		main.setResizable(false);
 		
 		main.setVisible(true);
